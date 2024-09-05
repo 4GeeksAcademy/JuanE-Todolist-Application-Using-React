2dd7c0636e3a94a52cf2ff42db6f533667a576e6
Juan E Gonzalez <158267755+juan007-play@users.noreply.github.com>
2024-09-05 05:20:21 +0000

    change-inputs-value-onchange

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,47 +3,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function TodoList() {
   const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
 
-  const addTask = (newTask) => {
-    setTasks([...tasks, newTask]);
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask.trim()]);
+      setNewTask('');
+    }
   };
-
   const removeTask = (index) => {
     const updatedTasks = [...tasks];
     updatedTasks.splice(index, 1);
     setTasks(updatedTasks);
   };
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
 
-  return (
-    <>
-      <div className="container">
-        <h1>Todos</h1>
-        <input
-          type="text"
-          placeholder="What needs to be done?"
-          className="form-control"
-          onKeyUp={(e) => {
-            if (e.key === 'Enter' && e.target.value.trim() !== '') {
-              addTask(e.target.value.trim());
-              e.target.value = '';
-            }
-          }}
-        />
-        <ul className="list-group ">
-          {tasks.map((task, index) => (
-            <li key={index} className="list-group-item d-flex justify-content-between">
-              {task}
-              <button className='delete-button' onClick={(onClick) => removeTask(index)}>x</button>
-            </li>
-          ))}
-          <li id='footer' className="list-group-item"></li>
-        </ul>
-      </div>
-      <div className="element">
-        <p>{tasks.length} item(s) left</p>
-      </div>
-    </>
-  );
+  const handleKeyUp = (e) => {
+    if (e.key === 'Enter') {
+      addTask();
+    }
+  }
+    return (
+      <>
+        <div className="container">
+          <h1>Todos</h1>
+          <input
+            type="text"
+            placeholder="What needs to be done?"
+            className="form-control"
+            value={newTask}
+            onChange={handleInputChange}
+            onKeyUp={handleKeyUp}
+          />
+          <ul className="list-group ">
+            {tasks.map((task, index) => (
+              <li key={index} className="list-group-item d-flex justify-content-between">
+                {task}
+                <button className='delete-button' onClick={(onClick) => removeTask(index)}>x</button>
+              </li>
+            ))}
+            <li id='footer' className="list-group-item"></li>
+          </ul>
+        </div>
+        <div className="element">
+          <p>{tasks.length} item(s) left</p>
+        </div>
+      </>
+    );
 }
 
-export default TodoList;
+  export default TodoList;
